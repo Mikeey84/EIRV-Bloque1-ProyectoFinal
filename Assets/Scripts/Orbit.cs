f7da3dd7b5dd3331127908659fb4ba2f26f77113
Mikeey84 <milope21@ucm.es>
2026-04-20 18:27:17 +0200

    Mejoras de escena

---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public Transform target;   
+    public float radius = 5f;  
+    public float speed = 250f;  
+
+    private float angle = 0f;
+
+    void Update()
+    {
+        if (target == null) return;
+
+        angle += speed * Time.deltaTime;
+
+        float rad = angle * Mathf.Deg2Rad;
+
+        float x = Mathf.Cos(rad) * radius;
+        float z = Mathf.Sin(rad) * radius;
+
+        transform.position = target.position + new Vector3(x, 0, z);
+    }
+}
