cf89bafb559cd412905813f0ea1dae8baaadeed6
MiguelDidactoons <milope21@ucm.es>
2026-04-24 18:58:59 +0200

    Escribe con sonidos

--- Assets/Scripts/DialogueSystems/InteractMessageScript.cs
+++ Assets/Scripts/DialogueSystems/InteractMessageScript.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -9,6 +10,9 @@ public class InteractMessageScript : MonoBehaviour
     [SerializeField] private CanvasGroup interactMessageCanvasGroup;
     [SerializeField] private TextMeshProUGUI interatMessageText;
     [SerializeField] private float displayTime = 5f;
+    [SerializeField] private float typewriterSpeed = 0.05f;
+    private AudioSource audioSource;
+    [SerializeField] private List<AudioClip> interactSounds;
 
     private void Awake()
     {
@@ -20,7 +24,20 @@ public class InteractMessageScript : MonoBehaviour
         Instance = this;
     }
 
+    private void Start()
+    {
+        interactMessageCanvasGroup.alpha = 0;
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void ShowMessage(string message, float displayTime = 5f)
+    {
+        interatMessageText.text = message;
+        StopAllCoroutines();
+        StartCoroutine(ShowInfoMessage());
+    }
+
+    public void ShowMessage(string message)
     {
         interatMessageText.text = message;
         StopAllCoroutines();
@@ -39,6 +56,40 @@ public class InteractMessageScript : MonoBehaviour
             interactMessageCanvasGroup.alpha = duration;
             yield return null;
         }
+        while (duration >= 0)
+        {
+            duration -= Time.deltaTime / (displayTime / 2);
+            interactMessageCanvasGroup.alpha = duration;
+            yield return null;
+        }
+        interatMessageText.text = "";
+        interactMessageCanvasGroup.gameObject.SetActive(false);
+    }
+
+    public void ShowMessageTypewriter(string message)
+    {
+        interatMessageText.text = message;
+        StopAllCoroutines();
+        StartCoroutine(ShowInfoMessageTypewriter());
+    }
+
+    IEnumerator ShowInfoMessageTypewriter()
+    {
+        string fullText = interatMessageText.text;
+        interatMessageText.text = "";
+        interactMessageCanvasGroup.alpha = 1;
+        interactMessageCanvasGroup.gameObject.SetActive(true);
+
+        foreach (char letter in fullText)
+        {
+            interatMessageText.text += letter;
+            audioSource.PlayOneShot(interactSounds[Random.Range(0, interactSounds.Count)]);
+            yield return new WaitForSeconds(typewriterSpeed);
+        }
+
+        yield return new WaitForSeconds(displayTime);
+
+        float duration = 1f;
         while (duration >= 0)
         {
             duration -= Time.deltaTime / (displayTime / 2);
